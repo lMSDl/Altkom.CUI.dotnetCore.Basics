f5f43b1baf9688c811dc631bb26b9709a1aaa328
Paweł Łęczycki <pawel@msd.info.pl>
2019-09-30 12:11:17 +0200

    Sekcje w plikach konfiguracyjnych

--- Core.Basics.Program/Program.cs
+++ Core.Basics.Program/Program.cs
@@ -6,24 +6,21 @@ namespace Core.Basics.Program
     class Program
     {
         public static IConfiguration Config = new ConfigurationBuilder()
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .AddXmlFile("appsettings.xml", optional: true, reloadOnChange: true)
-            .AddIniFile("appsettings.ini", optional: true, reloadOnChange: true)
-            .AddYamlFile("appsettings.yaml", optional: true, reloadOnChange: true)
+            //.AddIniFile("appsettings.ini", optional: true, reloadOnChange: true)
+            //.AddYamlFile("appsettings.yaml", optional: true, reloadOnChange: true)
             .Build();
 
         static void Main(string[] args)
         {
-            Hello(Config["HelloJson"]);
-            Hello(Config["HelloXml"]);
-            Hello(Config["HelloYaml"]);
-            Hello(Config["HelloIni"]);
+            Hello(Config["Section:Key2"], Config["Section:Subsection:Key1"]);
         }
 
-        private static void Hello(string from)
+        private static void Hello(string what, string from)
         {
             Console.WriteLine(
-                            Figgle.FiggleFonts.Standard.Render($"Hello {from}"));
+                            Figgle.FiggleFonts.Standard.Render($"{what} {from}"));
         }
     }
 }
