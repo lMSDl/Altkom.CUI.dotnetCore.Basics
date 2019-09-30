26e9ca317d045888730b0c58e8d6080bd0404de4
Paweł Łęczycki <pawel@msd.info.pl>
2019-09-30 12:01:59 +0200

    Pobieranie konfiguracji z pliku

--- Core.Basics.Program/Program.cs
+++ Core.Basics.Program/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
 
 namespace Core.Basics.Program
 {
     class Program
     {
+        public static IConfiguration Config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddXmlFile("appsettings.xml", optional: true, reloadOnChange: true)
+            .AddIniFile("appsettings.ini", optional: true, reloadOnChange: true)
+            .AddYamlFile("appsettings.yaml", optional: true, reloadOnChange: true)
+            .Build();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Class!");
+            Hello(Config["HelloJson"]);
+            Hello(Config["HelloXml"]);
+            Hello(Config["HelloYaml"]);
+            Hello(Config["HelloIni"]);
+        }
+
+        private static void Hello(string from)
+        {
+            Console.WriteLine(
+                            Figgle.FiggleFonts.Standard.Render($"Hello {from}"));
         }
     }
 }
