cad042100cf2d078f1094be39c3e226fd9c20083
Paweł Łęczycki <pawel@msd.info.pl>
2019-09-30 10:57:43 +0200

    Aplikacja konsolowa

---
+++ Core.Basics.Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.Basics.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Class!");
+        }
+    }
+}
