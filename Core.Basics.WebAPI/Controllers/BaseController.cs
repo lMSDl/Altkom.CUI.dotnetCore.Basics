774a5338cb4e1452d9ae0c9842c0a0fd76f81601
Paweł Łęczycki <pawel@msd.info.pl>
2019-10-01 09:53:12 +0200

    Utworzenie projektu WebAPI

---
+++ Core.Basics.WebAPI/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Basics.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class BaseController : ControllerBase
+    {
+    }
+}
